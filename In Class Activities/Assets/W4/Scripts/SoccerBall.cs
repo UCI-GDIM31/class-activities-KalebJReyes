57defee608e8e3635c21e43067af3215134709a5
KalebJReyes <kalebreyes.business@gmail.com>
2025-10-20 19:14:44 -0700

    Finished Activity and Devlog

--- In Class Activities/Assets/W4/Scripts/SoccerBall.cs
+++ In Class Activities/Assets/W4/Scripts/SoccerBall.cs
@@ -21,10 +21,10 @@ public class SoccerBall : MonoBehaviour
     //
     // Also, uncomment and FIX the Debug.Log line.
 
-    //private ?? ??
-    //{
+    private void OnTriggerEnter(Collider other)
+    {
         // finish STEP 1 by uncommenting and fixing the below line!
-        //Debug.Log(SoccerBall detected a collision with a trigger collider!);
+        
 
         // STEP 2 -------------------------------------------------------------
         // Write an IF STATEMENT to check if the game object we collided with
@@ -33,10 +33,12 @@ public class SoccerBall : MonoBehaviour
         //
         // Then, move your Debug.Log() statement so that it's only called if
         //      the colliding object has the "Goal" tag.
-
+        if (other.gameObject.tag.Equals("Goal")) {
+            MadeGoal();
+        }
 
         // STEP 2 -------------------------------------------------------------
-    //}
+    }
 
     // STEP 1 -----------------------------------------------------------------
 
@@ -49,17 +51,23 @@ public class SoccerBall : MonoBehaviour
     // Once you've created MadeGoal,
     //      move your Debug.Log() statement into MadeGoal, and
     //      call MadeGoal from inside your if statement in OnTriggerEnter.
-    
-        // STEP 4 -------------------------------------------------------------
-        // _goalVFX is a ParticleSystem, a Component for creating VFX.
-        // ParticleSystem has a method named Play() that displays the VFX:
-        // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/ParticleSystem.Play.html 
-        //
-        // Call Play on _goalVFX.
+    private void MadeGoal() {
+        Debug.Log("SoccerBall detected a collision with a trigger collider!");
+        _goalVFX.Play(true);
+        _points++;
+        _pointsText.text = "points: " + _points;
+        _timelast = 0.0f;
+    }
+    // STEP 4 -------------------------------------------------------------
+    // _goalVFX is a ParticleSystem, a Component for creating VFX.
+    // ParticleSystem has a method named Play() that displays the VFX:
+    // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/ParticleSystem.Play.html 
+    //
+    // Call Play on _goalVFX.
 
-        // STEP 4 -------------------------------------------------------------
-    
-    
+    // STEP 4 -------------------------------------------------------------
+
+
     // STEP 3 -----------------------------------------------------------------
 
 
@@ -76,9 +84,15 @@ public class SoccerBall : MonoBehaviour
     // You will need to:
     //      1. Create a new member variable to keep track of the points.
     //      2. Use your MadeGoal method to update the points and text.
-    //
+    private int _points = 0;
+
     // STEP 5 -----------------------------------------------------------------
-    
+    private float _timelast = 0.0f;
+
+    private void Update() {
+        _timelast += Time.deltaTime;
+        _timeText.text = "time since last goal: " + _timelast;
+    }
 
     // STEP 6 -----------------------------------------------------------------
     // Like the last step, these flags do NOT show you where to put all of the
--- README.md
+++ README.md
@@ -18,6 +18,12 @@ The parameters would be an integer to check the current level of friendship and 
 
 2. I think that the balls got extremely bright because the code we uncommented in step 9 changed the ball's saturation. Depending on their speed, the saturation gets set really high which causes the ball to become bright.
 
+### W4
+Table 11
+Line 17 declares a boolean member variable called "_isGrounded" and sets it to true. Line 28 is the start of an if statement that checks if the player pressed space and if the _isGrounded member variable is true. The if statement makes the cat jump. Line 32 sets "_isGrounded" to false. The "_isGrounded" variable is meant to show whether or not the cat is on the ground.
+
+1. The objects I gave rigid bodies to were the cat and the soccerball because we needed those to have physics, the cat needs to be able to jump and have gravity and the ball needs to be able to get kicked around and bounce. The goal has the is Trigger checked because we just want the collider to check whether or not the ball passed it. The goal did not need a physical collider.
+2. When I started testing my code, everything worked as intended on the first try. I did have some issues figuring out some parts of the code. For example, when making the OnTriggerEnter method, I forgot to declare what the method would return so I just had to add void since it doesn't return anything.
 ## Open-Source Assets
 ### W1
 - Animals: https://assetstore.unity.com/packages/3d/characters/animals/animals-free-animated-low-poly-3d-models-260727 
