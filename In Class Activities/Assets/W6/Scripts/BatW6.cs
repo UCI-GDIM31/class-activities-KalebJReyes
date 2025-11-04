2abb5d363882750fa3af0e96fa77ec3a32001588
KalebJReyes <kalebreyes.business@gmail.com>
2025-11-03 19:22:42 -0800

    BatW6 & Steps 1 - 3 (Unfinished)

--- In Class Activities/Assets/W6/Scripts/BatW6.cs
+++ In Class Activities/Assets/W6/Scripts/BatW6.cs
@@ -3,5 +3,29 @@ using UnityEngine;
 // Write the BatW6 class here.
 public class BatW6 : MonoBehaviour
 {
-    
+    [SerializeField] private float _speed;
+    public Transform _target;
+
+    private void Start() 
+    {
+        Chase();
+    }
+
+    private void Update() 
+    {
+        transform.position = Vector3.MoveTowards(transform.position, 
+                                                _target.position, 
+                                                _speed * Time.deltaTime);
+    }
+
+    public void Chase() 
+    {
+        enabled = true;
+
+    }
+
+    public void StopChase() 
+    {
+        enabled = false;
+    }
 }
